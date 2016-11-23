3c2bfe5b8496654016433f8e7bb528fabf9186b1
Steve Occhipinti <dev@stevenocchipinti.com>
2016-11-23 18:49:20 +1100

    Use a static module for SQS
    There doesn't seem to be any need to store state at this point and its
    an easy refactor to make it into a proper class if need be

--- sqs-cli.rb
+++ sqs-cli.rb
@@ -2,10 +2,8 @@ require "aws-sdk"
 require_relative "sqs"
 require_relative "cli"
 
-@sqs = SQS.new
-
 all_queue_urls = Cli.wait_with_message("Fetching queues...") do
-  @sqs.all_queues
+  SQS.all_queues
 end
 
 source = Cli.prompt "Source",
@@ -21,7 +19,7 @@ delete_when_done = Cli.prompt "Delete message when processed?"
 
 if source_url = source[:selected_option]
   count = 0
-  @sqs.read_message_batches(source_url) do |batch|
+  SQS.read_message_batches(source_url) do |batch|
 
     batch.each do |m|
       count += 1
@@ -29,8 +27,8 @@ if source_url = source[:selected_option]
     end
 
     if destination_url = destination[:selected_option]
-      @sqs.send_message_batches(destination_url, batch)
-      @sqs.delete_message_batches(source_url, batch) if delete_when_done
+      SQS.send_message_batches(destination_url, batch)
+      SQS.delete_message_batches(source_url, batch) if delete_when_done
     end
 
   end
--- sqs.rb
+++ sqs.rb
@@ -1,15 +1,13 @@
-class SQS
-  def initialize
-    @sqs = Aws::SQS::Client.new
-  end
+module SQS
+  extend self
 
   def all_queues
-    @sqs.list_queues.queue_urls
+    sqs.list_queues.queue_urls
   end
 
   def read_message_batches(queue_url, &block)
     loop do
-      resp = @sqs.receive_message(
+      resp = sqs.receive_message(
         queue_url: queue_url,
         max_number_of_messages: 10
       )
@@ -23,13 +21,19 @@ class SQS
     entries = batch.map do |msg|
       { id: msg.message_id, message_body: msg.body }
     end
-    @sqs.send_message_batch(queue_url: queue_url, entries: entries)
+    sqs.send_message_batch(queue_url: queue_url, entries: entries)
   end
 
   def delete_message_batches(queue_url, batch)
     entries = batch.map do |m|
       { id: m.message_id, receipt_handle: m.receipt_handle }
     end
-    @sqs.delete_message_batch(queue_url: queue_url, entries: entries)
+    sqs.delete_message_batch(queue_url: queue_url, entries: entries)
+  end
+
+  private
+
+  def self.sqs
+    @sqs ||= Aws::SQS::Client.new
   end
 end
