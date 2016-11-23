5d7f6f117039754aa99fb4510e8d2f142a2191c5
Steve Occhipinti <dev@stevenocchipinti.com>
2016-11-24 00:52:17 +1100

    Use Base64 for data files to handle newlines
    Storing "\n" separated Base64 string means that if the message itself
    has newlines, they will be encoded and stored verbatim.

--- lib/data_file.rb
+++ lib/data_file.rb
@@ -1,4 +1,5 @@
 require 'ostruct'
+require 'base64'
 
 class DataFile
   def initialize(filename)
@@ -10,7 +11,10 @@ class DataFile
     File.readlines(@filename).each_slice(10).each do |batch|
       yield (batch.map { |message|
         count += 1
-        OpenStruct.new(message_id: count.to_s, body: message)
+        OpenStruct.new(
+          message_id: count.to_s,
+          body: Base64.strict_decode64(message.strip)
+        )
       })
     end
   end
@@ -18,7 +22,7 @@ class DataFile
   def write_batch(batch)
     File.open(@filename, "a") do |f|
       batch.each do |message|
-        f.puts message.body
+        f.puts Base64.strict_encode64(message.body.strip)
       end
     end
   end
